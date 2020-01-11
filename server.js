31c3e3d3579d279498b63d33927a7b2a3e95c5fd
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-01-11 11:41:20 -0600

    server code

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express")
+var path = require("path")
+
+var app = express()
+var PORT = 3000
+var tableArray = []
+var waitListArray = []
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+class Reservation {
+    constructor(name, email, phone, ID) {
+        this.name = name
+        this.email = email
+        this.phone = phone
+        this.ID = ID
+    }
+}
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"))
+})
+
+app.get("/reserve", function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"))
+})
+
+app.get("/api/tables", function(req, res) {
+    res.json(tableArray)
+})
+
+app.get("api/waitlist", function(req, res) {
+    res.json(waitListArray)
+})
+
+app.post("/reserve", function(req, res) {
+    var data = req.body
+    var newReservation = new Reservation(data.name, data.email, data.phone, data.ID)
+    if (tableArray.length > 5){
+        waitListArray.push(newReservation)
+        console.log(waitListArray)
+    }
+    else {
+        tableArray.push(newReservation)
+        console.log(tableArray)
+    }
+})
+
+app.listen(PORT, function(){
+    console.log("I am listening, coder")
+})
